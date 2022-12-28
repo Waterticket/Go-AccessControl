0032e7f50ef58b5de266dfcd37b833afa2c260fa
Waterticket <matthew218@naver.com>
2022-12-29 00:51:00 +0900

    garbage collect modulize

--- main.go
+++ main.go
@@ -327,68 +327,7 @@ func main() {
 	}()
 
 	if config.Server.Master {
-		go func() {
-			for {
-				// Garbage Collection
-
-				lastProcessedIdx := getLastProcessedIdx()
-				buckets := zrangebyscoreBucket()
-				for _, bucket := range buckets {
-					idx := getIdxFromToken(s2b(bucket))
-					if idx > lastProcessedIdx {
-						lastProcessedIdx = idx
-					}
-
-					log.Println("[gc][remove] reqIdx=", idx)
-				}
-				lastProcessedIdxInRedis := getLastProcessedIdx()
-				//if lastProcessedIdx > lastProcessedIdxInRedis {
-				setLastProcessedIdx(lastProcessedIdx)
-				//}
-				log.Println("[gc][lastProcessedIdx] lastProcessedIdx=", lastProcessedIdx, "lastProcessedIdxInRedis=", lastProcessedIdxInRedis)
-
-				removedCount := zremrangebyscoreBucket()
-				log.Println("[gc] RemovedCount=", removedCount)
-
-				countOfBucket := getCountOfBucket()
-				log.Println("[gc] CountOfBucket=", countOfBucket)
-
-				var cnt = countOfBucket
-				var deletedIdx int64 = 0
-				for (config.Connection.AccessSize)*2 > cnt {
-					nextReq := popPendingQueue()
-					if len(nextReq) > 0 {
-						heartbeat := getHeartbeat(nextReq)
-						deletedIdx = getIdxFromToken(s2b(nextReq))
-						log.Println("[gc][pop] nextReq=", nextReq, "reqIdx=", deletedIdx)
-						if heartbeat < 0 {
-							//deletedIdx = getIdxFromToken(s2b(nextReq))
-							log.Println("[gc][pop][delete] nextReq=", nextReq, "reqIdx=", deletedIdx)
-							go func() {
-								// Heartbeat delete for prevent Memory Leak
-								delHeartbeat(nextReq)
-								setLastProcessedIdx(deletedIdx) // 무효키가 많으면 유용하나, 그 외에는 그닥?
-							}()
-							continue
-						}
-
-						zaddBucket(nextReq)
-						log.Println("[gc][pop][add] nextReq=", nextReq, "reqIdx=", deletedIdx)
-						cnt++
-					} else {
-						break
-					}
-				}
-
-				if deletedIdx > 0 {
-					setLastProcessedIdx(deletedIdx)
-				}
-
-				log.Println("[gc] AddedCount: ", cnt-countOfBucket)
-
-				time.Sleep(time.Millisecond * 100)
-			}
-		}()
+		go abnormalPackageGC()
 	}
 
 	initialCap, maxCap := config.Proxy.PoolConnectionCapacityMin, config.Proxy.PoolConnectionCapacityMax
